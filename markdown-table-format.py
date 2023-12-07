7c839a2a19a0aef2f02004f1ce7592a12b20d717
Scott Ramsay <7002566+smramsay@users.noreply.github.com>
2023-12-06 17:58:12 -0800

    Add test argument to `markdown-table-format.py`. It's been too long to immediately know why I made the other changes…

--- markdown-table-format.py
+++ markdown-table-format.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 """Formats markdown tables, though technically they are Pandoc markdown."""
 
-import argparse
 import fileinput
 
+from argparse import ArgumentParser
 from pathlib import Path
 from itertools import groupby
 
-parser = argparse.ArgumentParser()
+parser = ArgumentParser()
 parser.add_argument('file', type=Path,
                     help="The file with tables to fix.")
 parser.add_argument('-n', '--no-backup', action='store_true',
                     help="Disable backing up the file before writing.")
+parser.add_argument('-t', '--test', action='store_true',
+                    help="Write just the new tables to a separate file.")
 args = parser.parse_args()
+
+
+def is_dividing_line(line: str) -> bool:
+    # We require lines starting with '|' with make the first index empty
+    return line.split('|')[1][-1] in ['-', ':']
 
 
 def format_columns(table_lines: [str]) -> [str]:
@@ -24,22 +31,43 @@ def format_columns(table_lines: [str]) -> [str]:
         return table_lines
 
     # Count the number of columns in the header.
-    num_col = len(table_lines[0].strip('|').split('|'))
+    num_col = len(table_lines[0].split('|'))
     final_lines = table_lines
-    for column_idx in range(1, num_col + 1):
-        # Find the length of the longest entry in this column.
-        max_len = max([len(line.split('|')[column_idx]) for line in table_lines])
+    for column_idx in range(1, num_col):
+        # Find the length of the longest entry in this column that isn't a
+        # dividing line.
+        max_len = max(
+            [len(line.split('|')[column_idx].strip())
+             for line in table_lines
+             if len(line.split('|')) > column_idx
+             and not is_dividing_line(line)]
+        )
 
         # Pad the entries; special case for dividing line.
         new_lines = []
         for line in final_lines:
-            entries = line.split('|')
-            entry_len = len(entries[column_idx])
-            if entry_len < max_len:
-                fill = ' '
-                if entry_len != 0 and entries[column_idx][-1] == '-':
-                    fill = '-'
-                new_entry = entries[column_idx].ljust(max_len, fill)
+            entries = line.strip().split('|')
+            # Pad the number of entries to match the number of header columns
+            entries += [''] * (num_col - len(entries))
+            entry_text = entries[column_idx].strip()
+
+            # TODO: Use alignment indicators instead of justifying left.
+            if is_dividing_line(line):
+                # Reset the length to the minimum so we can remake it.
+                entry_text = ':-'
+
+            # TODO: Should work on all entry_text, right?
+            if len(entry_text) <= max_len:
+                fill = '-' if is_dividing_line(line) else ' '
+
+                # +1 to add space after longest entry
+                new_entry = entry_text.ljust(max_len + 1, fill)
+                if is_dividing_line(line):
+                    new_entry += '-'
+                else:
+                    # Adding a single space to separate it from the '|'
+                    new_entry = ' ' + new_entry
+
                 new_line = '|'.join(entries[:column_idx] + [new_entry] + entries[column_idx + 1:])
                 new_lines.append(new_line.strip())
             else:
@@ -80,6 +108,11 @@ def main():
     new_lines = []
     for table in get_tables(lines):
         new_lines += format_columns(table)
+
+    if args.test:
+        with open('/tmp/table-test.md', 'w') as testfile:
+            testfile.writelines([x + '\n' for x in new_lines])
+        return
 
     replace_tables_inplace(args.file, new_lines)
 
